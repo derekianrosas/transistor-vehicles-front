732b7017eb6c8b174b126ffb8ee28281979a0372
derekrosas <derekianrosas@gmail.com>
2020-07-14 12:26:08 -0400

    initial code for loginjs

---
+++ src/components/login/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function login(props) {
+  const errorMessage = {
+    none: "",
+    "blank field": "Please fill in all fields.",
+    "fetch error": "An error occured. Please try again later.",
+    "not verified": "Incorrect username or password.",
+  };
+}
